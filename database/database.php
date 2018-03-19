0fdeff239e83f0e898429ddaf658c19dd47dd4bc
Josh <joshuahansen188@gmail.com>
2018-03-19 11:43:56 +1100

    database singleton class

---
+++ database/database.php
@@ -0,0 +1,44 @@
+<?php
+	class Database
+	{
+		private static $instance = null;
+		private $conn;
+
+		private $host = 'localhost';
+		private $userName = 'jzl-carshare';
+		private $pass = 'root';
+		private $dbname = 'jzl-carshare';
+
+		private function __construct()
+		{
+			$this->conn = new mysqli($this->host, $this->userName, $this->pass,
+									$this->dbname);
+			if($this->conn->connect_error)
+			{
+				die("Connection failed: " .$this->conn->connect_error);
+			}
+		}
+	
+		private function __clone() { }
+	
+		public static function getInstance()
+		{
+			if(!self::$instance)
+			{
+				self::$instance = new Database();
+			}
+			
+			return self::$instance;
+		}
+
+		public function getConnection()
+		{
+			return $this->conn;
+		}
+		
+		public function closeConnection()
+		{
+			$this->conn->close();
+		}	
+	}
+?>
