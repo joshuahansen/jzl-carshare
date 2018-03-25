4c4b95096400dadc283e3a45c17909cfdf4f8daa
Lohgan Nash <lohgannash@gmail.com>
2018-03-25 18:07:40 +1100

    Module files
    The modules files for the head, nav and footer. Should be now able to include these with php.

---
+++ view/modules/head.php
@@ -0,0 +1,8 @@
+<head>
+        <meta charset="utf-8">
+        <title>JZL Carshare | Home</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Lobster|Open+Sans|Poiret+One|Rajdhani|Six+Caps" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+</head> 
