fceee67594d36c947b0884168ecb8ea49b7a3247
Josh <joshua.hansen188@gmail.com>
2018-04-11 23:09:25 +1000

    dashboard map centers and displays users position

--- view/dashboard.php
+++ view/dashboard.php
@@ -1,19 +1,25 @@
 <div class='container' id="googleMap" style="height:500px; width:1000px"></div>
 <script>
 function myMap() {
-    var mapCenter = new google.maps.LatLng(-37.818470, 144.953579);
-    var mapCanvas = document.getElementById('googleMap');
-    var mapOptions = {center: mapCenter, zoom:12};
-    var map = new google.maps.Map(mapCanvas,mapOptions);
-    
-    var locations = <?php echo json_encode($locations);?>;
-     
-    for(var i = 0; i < locations.length; ++i)
+    if(navigator.geolocation)
     {
-        var locat = locations[i];
-        var markerPos = new google.maps.LatLng(locat['longtitude'],locat['latitude']);
-        var marker = new google.maps.Marker({position: markerPos});
-        marker.setMap(map);
+        navigator.geolocation.getCurrentPosition(function (position){
+            var lon = position.coords.longitude;
+            var lat = position.coords.latitude;
+            var mapCenter = new google.maps.LatLng(lat, lon);
+            var mapCanvas = document.getElementById('googleMap');
+            var mapOptions = {center: mapCenter, zoom:12};
+            var map = new google.maps.Map(mapCanvas,mapOptions);
+            var marker = new google.maps.Marker({position: mapCenter});
+            marker.setMap(map);
+        });
+    }
+    else
+    {
+        var mapCenter = new google.maps.LatLng(-37.818470, 144.953579);
+        var mapCanvas = document.getElementById('googleMap');
+        var mapOptions = {center: mapCenter, zoom:12};
+        var map = new google.maps.Map(mapCanvas,mapOptions);
     }
 }
 </script>
