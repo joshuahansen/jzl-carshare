e22e4c082a5614798a5af613a61d8ccc0caf78ec
Lohgan Nash <lohgannash@gmail.com>
2018-04-13 00:26:57 +1000

    Home page and login
    Home page view and login view, Register modal and passing info to controller not completed yet

---
+++ view/login.php
@@ -0,0 +1,15 @@
+<div class="login-container">
+    <div class="card card-container">
+    <h2 class='login-title text-center'>Login</h2>
+    <hr class="loginhr">
+        <form class="form-signin">
+            <p class="input-title">Email</p>
+            <input type="text" id="email" class="login-box" placeholder="email@email.com" required autofocus>
+            <p class="input-title">Password</p>
+            <input type="password" id="password" class="pword-box" placeholder="******" required>
+            <a class="forgot" href="">Forgotten password?</a>
+            <button class="btn btn-lg btn-primary" type="submit">Login</button>
+            <button class="btn btn-lg btn-secondary" type="submit">Register</button>
+        </form>
+    </div>
+</div>
