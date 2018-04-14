b2d1066c276eb47500d91574efb6da415d061411
Josh <joshua.hansen188@gmail.com>
2018-04-14 23:08:09 +1000

    added sidenav to dashboard page

--- view/modules/head.php
+++ view/modules/head.php
@@ -3,6 +3,10 @@
         <title>JZL Carshare | Home</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>`
         <link href="https://fonts.googleapis.com/css?family=Lobster|Open+Sans|Poiret+One|Rajdhani|Six+Caps" rel="stylesheet">
         <link rel="stylesheet" href="view/css/style.css">
+        <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
+
 </head> 
