902cec92e6bfb71e20fdf0a28ac5e15328b0f22f
joshuahansen <joshua.hansen188@gmail.com>
2018-06-04 08:23:07 +1000

    added return location booking still needs testing

---
+++ view/book-location.php
@@ -0,0 +1,26 @@
+<?php
+    require_once('controller/LoanController.php');
+    require_once('database/databaseController.php');
+    $loanController = LoanController::getInstance();
+    $databaseController = DatabaseController::getInstance();
+
+    $currentLoan = $loanController->getCurrentLoan();
+
+    $loanId = $currentLoan->getLoanId();
+    $locationId = $_POST['book-locationId'];
+
+    $booked = $databaseController->bookLocation($locationId, $loanId);
+
+    if($booked)
+    {
+        echo "location booked";
+    }
+    else
+    {
+        echo "unable to book location";
+    }
+?>
+    
+    
+    
+    
