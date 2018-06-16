dbf86235bbea54a4a89b1597e78b11f7575ecbfe
Lohgan Nash <lohgannash@gmail.com>
2018-06-16 15:04:54 +1000

    Fixes (finally)

--- view/modules/nav.php
+++ view/modules/nav.php
@@ -1,37 +1,32 @@
 <nav class="navbar navbar-light navbar-expand-lg" id="nav">
-    <div class="container">
-        <a class="navbar-brand" href="/JZL-carshare/">JZL Carshare</a>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <div class="dropdown">
-                        <a class="nav-link" href="">About Us</a>
-                        <div class="dropdown-content">
-                            <a href="story">Our Story</a>
-                            <a href="cars">Our Cars</a>
-                            <a href="locations">Our Locations</a>
-                        </div>
-                    </div>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="dashboard">Dashboard</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="contact">Contact Us</a>
-                </li>
-                <li class="nav-item">
-                    <?php
-                        if(isset($_SESSION['currentUser']))
-                        {
-                            echo '<a class="nav-link" href="logout">Log Out</a>';
-                        }
-                        else
-                        {
-                            echo '<a class="nav-link" href="login">Log In</a>';
-                        }
-                    ?>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+   <div class="container">
+       <div class="navbar-header">
+           <a class="navbar-brand" href="/JZL-carshare/">JZL Carshare</a>
+           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+           <span class="navbar-toggler-icon"></span>
+ </button>
+       </div>
+       <div id="navbarSupportedContent" class="navbar-collapse collapse">
+           <ul class="navbar-nav ml-auto">
+               <li class="nav-item">
+                   <div class="dropdown">
+                       <a class="nav-link" href="">About Us</a>
+                       <div class="dropdown-content">
+                           <a href="cars">Our Cars</a>
+                           <a href="locations">Our Locations</a>
+                       </div>
+                   </div>
+               </li>
+               <li class="nav-item">
+                 <a class="nav-link" href="dashboard">Dashboard</a>
+               </li>
+                 <li class="nav-item">
+                     <?php
+                         if(isset($_SESSION['currentUser']))
+                     ?>
+                 </li>
+             </ul>
+
+         </div>
+     </div>
+</nav> 
